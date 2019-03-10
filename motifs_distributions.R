2ec3e6cc96f8ffd3d0a031604add20b01b01fbe2
Chante Bethell <chantebethell@gmail.com>
2019-03-10 15:36:13 -0400

    attempt at heatmap plot

--- motifs_distributions.R
+++ motifs_distributions.R
@@ -1,5 +1,6 @@
 #analysis of motifs distributions 
 library(tidyverse)
+
 data <- read_csv("human_proteome_motifs_across_domains.csv")
 
 #focus only on the motif "YXX[LIMFV] and find the number of motifs in each domain type: ordered and disordered for each protein 
@@ -12,7 +13,12 @@ YXX <- data %>%
 ### One solution is geom_hex: https://ggplot2.tidyverse.org/reference/geom_hex.html . This makes a heatmap which can be colored by number of proteins at each point. Try that out for Monday.
 
 YXX %>%
-  ggplot(aes(count,domain_type)) +
-  geom_hex()
+  as_tibble() %>% 
+  ggplot(aes(x = sequence_id, y = domain_type)) +
+  geom_hex(aes(color = count)) 
+
+
+
+#cluster to include sequence ids (since amount of observations seems to be too large)?
 
 #find proteins that exist in only one of the datasets (does not overlap)
