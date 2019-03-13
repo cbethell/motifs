9752089c3ea1c9042ae2daf7ebf941871c8610f7
Chante Bethell <chantebethell@gmail.com>
2019-03-13 05:21:36 -0400

    heatmap plots with labeled axis and title

--- motifs_distributions.R
+++ motifs_distributions.R
@@ -1,5 +1,6 @@
 #analysis of motifs distributions 
 library(tidyverse)
+library(scales)
 data <- read_csv("human_proteome_motifs_across_domains.csv")
 
 #focus only on the motif "YXX[LIMFV] and find the number of motifs in each domain type: ordered and disordered for each protein 
@@ -10,13 +11,27 @@ YXX <- data %>%
   summarize(count=n()) %>%
   ungroup() %>%
   spread(domain_type, count) %>%
-  replace_na(list(D = 0, O = 0))
+  replace_na(list(D = 0, O = 0)) %>%
+  mutate(sum = D + O)
 
-YXX_plot <- YXX %>%
-  as_tibble() %>%
+#heatmap of atleast one motif in the ordered or disordered region 
+YXX_general_plot <- YXX %>%
+  filter(sum > 0) %>%
   ggplot(aes(x = O, y = D)) + 
-  geom_hex()
+  scale_x_continuous("Motif Count in Ordered Regions", breaks = pretty_breaks()) +
+  scale_y_continuous("Motif Count in Disordered Regions") +
+  labs(title = "Motif Counts") +
+  geom_hex(bins = 10) +
+  stat_binhex(aes(label=..count..), geom="text", bins=10, colour="white")
 
-#cluster to include sequence ids (since amount of observations seems to be too large)?
+#heatmap of atleast one motif in each domain 
+YXX_domain_plot <- YXX %>%
+  filter(D > 0) %>%
+  filter(O > 0) %>%
+  ggplot(aes(x = O, y = D)) + 
+  scale_x_continuous("Motif Count in Ordered Regions", breaks = pretty_breaks()) +
+  scale_y_continuous("Motif Count in Disordered Regions") +
+  labs(title = "Motif Counts where each Domain has at least one") +
+  geom_hex(bins = 10) +
+  stat_binhex(aes(label=..count..), geom="text", bins=10, colour="white") 
 
-#find proteins that exist in only one of the datasets (does not overlap)
