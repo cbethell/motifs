d624c2078404adcd79f89a30d081a3c9176e8cc8
Chante Bethell <chantebethell@gmail.com>
2019-03-22 18:41:22 -0400

    created separated domain plots and combined them using cow_plot

--- motifs_distributions.R
+++ motifs_distributions.R
@@ -1,11 +1,8 @@
 #analysis of motifs distributions 
 library(tidyverse)
 library(scales)
+library(cowplot)
 data <- read_csv("human_proteome_motifs_across_domains.csv")
-
-### Also note.. some of the motifs here are subsets of the others, i.e. "DLL" is in "XL[LI]". Let's make a faceted plot without some of the duplicates, i.e. you'd exclude DLL but keep the motif that contains it. You should look into this manually, pick the specific ones to keep.
-
-
 
 #focus only on the motif "YXX[LIMFV] and find the number of motifs in each domain type: ordered and disordered for each protein 
 
@@ -44,29 +41,46 @@ YXX_domain_plot <- YXX %>%
 
 ggsave(file = "YXX_domain_plot.pdf", YXX_domain_plot)
 
-
-motif_domain_plot <- data %>%
+#plot showing count of motifs in disordered regions for each motif type
+motif_disordered_plot <- data %>%
   group_by(motif_type,sequence_id,domain_type) %>%
   summarize(count = n()) %>%
   spread(domain_type, count) %>%
   replace_na(list(D = 0, O = 0)) %>%
-  filter(D > 0) %>%
-  filter(O > 0) %>%
+  filter(D > 0, O > 0) %>%
   group_by(motif_type) %>%
   filter(motif_type != "DLL") %>%
   filter(motif_type != "LLX") %>%
-  ggplot(aes(x = O, y = D)) + 
-  scale_x_continuous("Motif Count in Ordered Regions", breaks = pretty_breaks()) +
-  scale_y_continuous("Motif Count in Disordered Regions", breaks = pretty_breaks()) +
-  labs(title = "Motif Counts where each Domain has at least one") +
-  geom_hex(bins = 5) +
-  stat_binhex(aes(label=..count..), geom="text", bins=5, colour="white") +
-  facet_wrap(~motif_type, ncol = 3)
-  
-ggsave(file = "motif_domain_plot.pdf", motif_domain_plot)
+  ggplot(aes(x = motif_type, y = D)) + 
+  theme(axis.text.x = element_text(size = 5)) +
+  geom_hex(bins = 10) +
+  stat_binhex(aes(label=..count..), geom="text", bins=10, colour="white",size = 3)
 
+ggsave(file = "motif_disordered_plot.pdf", motif_disordered_plot)
+
+#plot showing count of motifs in ordered regions for each motif type
+motif_ordered_plot <- data %>%
+  group_by(motif_type,sequence_id,domain_type) %>%
+  summarize(count = n()) %>%
+  spread(domain_type, count) %>%
+  replace_na(list(D = 0, O = 0)) %>%
+  filter(D > 0, O > 0) %>%
+  group_by(motif_type) %>%
+  filter(motif_type != "DLL") %>%
+  filter(motif_type != "LLX") %>%
+  ggplot(aes(x = motif_type, y = O)) + 
+  theme(axis.text.x = element_text(size = 5)) +
+  geom_hex(bins = 10) +
+  stat_binhex(aes(label=..count..), geom="text", bins=10, colour="white",size = 2.5)
+
+ggsave(file = "motif_ordered_plot.pdf", motif_ordered_plot)
+
+#cowplot to compare the disordered and ordered plots
+combined_domain_plot <- plot_grid(motif_disordered_plot, motif_ordered_plot, labels = "auto")
+ggsave(file = "combined_domain_plot.pdf", combined_domain_plot)
+
+#read in endocytosis protein data
 endo_data <- read_csv("endocytosis_involved_proteins.csv")
-
 data <- rename(data, UNIPROT_ID = sequence_id)
 
 endo_proteins <- endo_data %>%
@@ -78,9 +92,9 @@ endo_proteins <- endo_data %>%
   summarize(count = n()) %>%
   spread(domain_type, count) %>%
   replace_na(list(D = 0, O = 0)) %>%
-  filter(D > 0) %>%
-  filter(O > 0) %>%
-  group_by(motif_type)
+  filter(D > 0, O > 0) %>%
+  group_by(motif_type) %>%
+  ungroup()
   
 
 non_endo_proteins <- data %>%
@@ -92,10 +106,18 @@ non_endo_proteins <- data %>%
   summarize(count = n()) %>%
   spread(domain_type, count) %>%
   replace_na(list(D = 0, O = 0)) %>%
-  filter(D > 0) %>%
-  filter(O > 0) %>%
-  group_by(motif_type)
+  filter(D > 0, O > 0) %>%
+  group_by(motif_type) %>%
+  ungroup()
 
+definite_active_motifs <- data %>%
+  filter(UNIPROT_ID == "O95782" | UNIPROT_ID == "O94973" | UNIPROT_ID == "P63010" | UNIPROT_ID == "P53680" | UNIPROT_ID == "Q96CW1" | UNIPROT_ID == "Q2M2I8
+") %>%
+  group_by(UNIPROT_ID,domain_type) %>%
+  summarize(count=n()) %>%
+  spread(domain_type, count) %>%
+  ggplot(aes(x = O, y = D)) + 
+  geom_hex(bins = 10) 
+  
 
-#compare proteins in endocytosis file to proteins not in endocytosis file(but in human proteome orginal file)
 # Focus further on these proteins which are specifically involved in forming clathrin-coated pits for endocytosis: AP2A1, AP2A2, AP2B1, AP2S1, AP2M1, AAK1.You can use summarize or tally functions to determine the counts of motifs per structural domain in each of these proteins.
