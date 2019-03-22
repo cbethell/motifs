fd906c6742ad0fdfacfa43c3ac0d52a96b5170a7
Chante Bethell <chantebethell@gmail.com>
2019-03-22 01:00:44 -0400

    script using ggsave and attempting the task to compare motifs of endocytotic vs non-endocytotic proteins

--- motifs_distributions.R
+++ motifs_distributions.R
@@ -2,9 +2,7 @@
 library(tidyverse)
 library(scales)
 data <- read_csv("human_proteome_motifs_across_domains.csv")
-## Notes..
-### Use the "ggsave()" function to plot instead of base R. it works more reliably w/ plots made by ggplot
-### Your full plot is totally fine - it's just an issue of dimensions. Try facet_wrap() where you can specify #rows, columns. Might work better? We'll see. It may be too busy anyways. There may be a better way to visualize about this data.
+
 ### Also note.. some of the motifs here are subsets of the others, i.e. "DLL" is in "XL[LI]". Let's make a faceted plot without some of the duplicates, i.e. you'd exclude DLL but keep the motif that contains it. You should look into this manually, pick the specific ones to keep.
 
 
@@ -30,9 +28,8 @@ YXX_general_plot <- YXX %>%
   geom_hex(bins = 10) +
   stat_binhex(aes(label=..count..), geom="text", bins=10, colour="white")
 
-pdf("YXX_general_plot.pdf")
-YXX_general_plot
-dev.off()
+ggsave(file = "YXX_general_plot.pdf", YXX_general_plot)
+
 
 #heatmap of atleast one motif in each domain 
 YXX_domain_plot <- YXX %>%
@@ -45,9 +42,8 @@ YXX_domain_plot <- YXX %>%
   geom_hex(bins = 10) +
   stat_binhex(aes(label=..count..), geom="text", bins=10, colour="white") 
 
-pdf("YXX_domain_plot.pdf")
-YXX_domain_plot
-dev.off()
+ggsave(file = "YXX_domain_plot.pdf", YXX_domain_plot)
+
 
 motif_domain_plot <- data %>%
   group_by(motif_type,sequence_id,domain_type) %>%
@@ -57,24 +53,49 @@ motif_domain_plot <- data %>%
   filter(D > 0) %>%
   filter(O > 0) %>%
   group_by(motif_type) %>%
+  filter(motif_type != "DLL") %>%
+  filter(motif_type != "LLX") %>%
   ggplot(aes(x = O, y = D)) + 
   scale_x_continuous("Motif Count in Ordered Regions", breaks = pretty_breaks()) +
   scale_y_continuous("Motif Count in Disordered Regions", breaks = pretty_breaks()) +
   labs(title = "Motif Counts where each Domain has at least one") +
   geom_hex(bins = 5) +
   stat_binhex(aes(label=..count..), geom="text", bins=5, colour="white") +
-  facet_grid(motif_type ~ .)
+  facet_wrap(~motif_type, ncol = 3)
   
-pdf("motif_domain_plot.pdf")
-motif_domain_plot
-dev.off()
-
-#need to fix the scaling and visuals?
+ggsave(file = "motif_domain_plot.pdf", motif_domain_plot)
 
 endo_data <- read_csv("endocytosis_involved_proteins.csv")
 
-compare_proteins_plot <- data %>%
-  endo_data %>%
-  filter()
+data <- rename(data, UNIPROT_ID = sequence_id)
+
+endo_proteins <- endo_data %>%
+  select(UNIPROT_ID) %>%
+  mutate(endo = UNIPROT_ID %in% data$UNIPROT_ID) %>%
+  filter(endo == TRUE) %>%
+  inner_join(data, by = c('UNIPROT_ID')) %>%
+  group_by(UNIPROT_ID,motif_type,domain_type) %>%
+  summarize(count = n()) %>%
+  spread(domain_type, count) %>%
+  replace_na(list(D = 0, O = 0)) %>%
+  filter(D > 0) %>%
+  filter(O > 0) %>%
+  group_by(motif_type)
+  
+
+non_endo_proteins <- data %>%
+  select(UNIPROT_ID) %>%
+  mutate(non_endo = UNIPROT_ID %in% endo_data$UNIPROT_ID) %>%
+  filter(non_endo == FALSE) %>%
+  inner_join(data, by = c('UNIPROT_ID')) %>%
+  group_by(UNIPROT_ID,motif_type,domain_type) %>%
+  summarize(count = n()) %>%
+  spread(domain_type, count) %>%
+  replace_na(list(D = 0, O = 0)) %>%
+  filter(D > 0) %>%
+  filter(O > 0) %>%
+  group_by(motif_type)
+
+
 #compare proteins in endocytosis file to proteins not in endocytosis file(but in human proteome orginal file)
 # Focus further on these proteins which are specifically involved in forming clathrin-coated pits for endocytosis: AP2A1, AP2A2, AP2B1, AP2S1, AP2M1, AAK1.You can use summarize or tally functions to determine the counts of motifs per structural domain in each of these proteins.
